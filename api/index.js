768c0de4f97b6bc0027ed35ed9bfa8ba6fe0efba
Abdul Samad <50093236+abdulsamad100@users.noreply.github.com>
2025-01-25 17:03:47 +0500

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { authroutes } = require("../routes/auth-routes");
+
+dotenv.config();
+
+const app = express();
+const MONGOURL = process.env.MONGO_URL;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// MongoDB Connection
+mongoose
+    .connect(MONGOURL)
+    .then(() => {
+        console.log("MONGO_DB Connected");
+    })
+    .catch((e) => {
+        console.log("MongoDB Connection Error: ", e.message);
+    });
+
+// Routes
+app.use("/auth", authroutes);
+
+app.get("/", (req, res) => {
+    res.json({ meessage: "Server Running" });
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
